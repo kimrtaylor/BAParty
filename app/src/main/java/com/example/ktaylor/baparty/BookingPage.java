16bdaeae357fd83b06a4888187bac4e5a06e2a6c
kimritaylor <ktaylor@and.digital>
2018-09-20 13:10:31 +0100

    Let's get this party started! 🎉

---
+++ app/src/main/java/com/example/ktaylor/baparty/BookingPage.java
@@ -0,0 +1,13 @@
+package com.example.ktaylor.baparty;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BookingPage extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_booking_page);
+    }
+}
