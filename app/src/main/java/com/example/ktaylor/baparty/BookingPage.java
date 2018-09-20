94d1c4678aa6c4e6bea2b501b587944ed2aff5fe
katemoksina <kmoksina@and.digital>
2018-09-20 15:01:35 +0100

    sharing + fullscreen + screenshot background + aligned button on BookingPage

--- app/src/main/java/com/example/ktaylor/baparty/BookingPage.java
+++ app/src/main/java/com/example/ktaylor/baparty/BookingPage.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 
 public class BookingPage extends Activity {
@@ -11,8 +13,11 @@ public class BookingPage extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_booking_page);
-        Button button = findViewById(R.id.button);
+        Button button = findViewById(R.id.party_button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
--- app/src/main/java/com/example/ktaylor/baparty/PartyPage.java
+++ app/src/main/java/com/example/ktaylor/baparty/PartyPage.java
@@ -1,13 +1,32 @@
 package com.example.ktaylor.baparty;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PartyPage extends Activity {
+
+    String link = "https://www.ba.com";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_party_page);
+        final Button button = findViewById(R.id.invite_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+                sharingIntent.setType("text/plain");
+                String shareBody = "Hey, I just booked a BA Flight to Ibiza! Come join me! " + link;
+                String shareSub = "Your subject here";
+                sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, shareSub);
+                sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
+                startActivity(Intent.createChooser(sharingIntent, "Share the party!"));
+
+            }
+        });
     }
 }
