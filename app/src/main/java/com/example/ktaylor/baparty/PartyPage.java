a9c2549f8c5b1af3b5469dd40f21cb8397c910fe
katemoksina <kmoksina@and.digital>
2018-09-20 16:29:52 +0100

    adding party bg

--- app/src/main/java/com/example/ktaylor/baparty/PartyPage.java
+++ app/src/main/java/com/example/ktaylor/baparty/PartyPage.java
@@ -6,15 +6,23 @@ import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 
 public class PartyPage extends Activity {
 
-    String link = "https://www.ba.com";
+    String link = "https://partywebapp.firebaseapp.com/";
+    String messageText = "Hey, I just booked a BA Flight to Ibiza! Come join me! ";
+    String messageSubject = "Your subject here";
+    String sharerTitle = "Share the party!";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_party_page);
         final Button button = findViewById(R.id.invite_button);
         final MediaPlayer mPlayer = MediaPlayer.create(this.getApplicationContext(), R.raw.come_fly_with_me);
@@ -24,11 +32,11 @@ public class PartyPage extends Activity {
             public void onClick(View v) {
                 Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
                 sharingIntent.setType("text/plain");
-                String shareBody = "Hey, I just booked a BA Flight to Ibiza! Come join me! " + link;
-                String shareSub = "Your subject here";
+                String shareBody = messageText + link;
+                String shareSub = messageSubject;
                 sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, shareSub);
                 sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
-                startActivity(Intent.createChooser(sharingIntent, "Share the party!"));
+                startActivity(Intent.createChooser(sharingIntent, sharerTitle));
 
                 mPlayer.stop();
             }
