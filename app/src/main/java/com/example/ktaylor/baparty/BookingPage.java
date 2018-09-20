b2e2f33d43de8bf0878ace7c8e37d235acc5381e
katemoksina <kmoksina@and.digital>
2018-09-20 13:53:45 +0100

    added 2nd activity

--- app/src/main/java/com/example/ktaylor/baparty/BookingPage.java
+++ app/src/main/java/com/example/ktaylor/baparty/BookingPage.java
@@ -1,7 +1,10 @@
 package com.example.ktaylor.baparty;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class BookingPage extends Activity {
 
@@ -9,5 +12,14 @@ public class BookingPage extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_booking_page);
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Intent intent = new Intent(BookingPage.this, PartyPage.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
---
+++ app/src/main/java/com/example/ktaylor/baparty/PartyPage.java
@@ -0,0 +1,13 @@
+package com.example.ktaylor.baparty;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PartyPage extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_party_page);
+    }
+}
