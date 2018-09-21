772873b213df645880a7816906961194f53ec9bf
katemoksina <kmoksina@and.digital>
2018-09-21 12:17:30 +0100

    video playing + style fixes + confetti background + header image on party page

--- app/src/main/java/com/example/ktaylor/baparty/BookingPage.java
+++ app/src/main/java/com/example/ktaylor/baparty/BookingPage.java
@@ -13,7 +13,6 @@ public class BookingPage extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_booking_page);
--- app/src/main/java/com/example/ktaylor/baparty/PartyPage.java
+++ app/src/main/java/com/example/ktaylor/baparty/PartyPage.java
@@ -1,26 +1,35 @@
 package com.example.ktaylor.baparty;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class PartyPage extends Activity {
+
+    private VideoView myVideoView;
+    private int position = 0;
+    private ProgressDialog progressDialog;
+    private MediaController mediaControls;
 
     String link = "https://partywebapp.firebaseapp.com/";
     String messageText = "Hey, I just booked a BA Flight to Ibiza! Come join me! ";
     String messageSubject = "Your subject here";
-    String sharerTitle = "Share the party!";
+    String sharerTitle = "Invite your friends!";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_party_page);
@@ -42,6 +51,57 @@ public class PartyPage extends Activity {
             }
         });
 
+        //pls no one judge the following code this is copied, we have no time
+
+        //set the media controller buttons
+        if (mediaControls == null) {
+            mediaControls = new MediaController(PartyPage.this);
+        }
+
+        //initialize the VideoView
+        myVideoView = (VideoView) findViewById(R.id.video_view);
+
+        // create a progress bar while the video file is loading
+        progressDialog = new ProgressDialog(PartyPage.this);
+        // set a title for the progress bar
+        progressDialog.setTitle("JavaCodeGeeks Android Video View Example");
+        // set a message for the progress bar
+        progressDialog.setMessage("Loading...");
+        //set the progress bar not cancelable on users' touch
+        progressDialog.setCancelable(false);
+        // show the progress bar
+        progressDialog.show();
+
+        try {
+            //set the media controller in the VideoView
+            myVideoView.setMediaController(mediaControls);
+
+            //set the uri of the video to be played
+            myVideoView.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.ibiza_test1));
+
+        } catch (Exception e) {
+            Log.e("Error", e.getMessage());
+            e.printStackTrace();
+        }
+
+        myVideoView.requestFocus();
+        //we also set an setOnPreparedListener in order to know when the video file is ready for playback
+        myVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                // close the progress bar and play the video
+                progressDialog.dismiss();
+                //if we have a position on savedInstanceState, the video playback should start from here
+                myVideoView.seekTo(position);
+                if (position == 0) {
+                    myVideoView.start();
+                } else {
+                    //if we come from a resumed activity, video playback will be paused
+                    myVideoView.pause();
+                }
+            }
+        });
 
     }
-}
+    }
+
